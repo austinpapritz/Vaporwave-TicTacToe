c0ae895dcd90f411a587c8466f0d249a780fc486
Austin Papritz <austin@papritz.dev>
2023-01-26 11:21:03 -0800

    added currentplayer and gamemessage states which are logging to console in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useContext } from 'react';
 import './App.css';
 
 import Board from './components/Board.js';
+import { GameContext } from './context/GameContext.js';
 
 function App() {
+  const { gamemessage, currentplayer } = useContext(GameContext);
+
+  console.log('gamemessage', gamemessage);
+  console.log('currentplayer', currentplayer);
+
   return (
     <div className="App">
       <div className="alpha-container">
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -3,9 +3,17 @@ import { createContext, useState } from 'react';
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
-  const [board, setBoard] = useState(Array(9).fill('X'));
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [currentplayer, setCurrentPlayer] = useState('X');
+  const [gamemessage, setGameMessage] = useState('Turn: X');
 
-  return <GameContext.Provider value={{ board, setBoard }}>{children}</GameContext.Provider>;
+  return (
+    <GameContext.Provider
+      value={{ board, setBoard, currentplayer, setCurrentPlayer, gamemessage, setGameMessage }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
 };
 
 export { GameContext, GameProvider };
